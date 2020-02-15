dccd5b3331efcb3ecbafc9a6a14628e66c6beb4c
Luz Divina Delgado Castañeda <luzdivdelg@gmail.com>
2020-02-15 21:51:53 +0100

    style fixed

--- starter-code/configs/preformatter.config.js
+++ starter-code/configs/preformatter.config.js
@@ -2,8 +2,8 @@ const path = require('path');
 
 module.exports = app => {
     app.use(require('node-sass-middleware')({
-        src: path.join(__dirname, 'public'),
-        dest: path.join(__dirname, 'public'),
+        src: path.join(__dirname, '..', 'public'),
+        dest: path.join(__dirname, '..', 'public'),
         sourceMap: true
     }));
 }
