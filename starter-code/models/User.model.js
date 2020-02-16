63ef0b4122a1882c661e30c567ba18e922f45032
Luz Divina Delgado Castañeda <luzdivdelg@gmail.com>
2020-02-16 20:42:16 +0100

    user model created

---
+++ starter-code/models/User.model.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: String,
+  password: String,
+  email: String,
+  avatar: String,
+  genre: Array,
+}, {
+  timestamps: true
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
