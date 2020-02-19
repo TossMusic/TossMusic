935a1e97a389ab924cb556bff2435f92e2b92715
Sara Garzon <saragarzonburgos@gmail.com>
2020-02-19 18:47:04 +0100

    nada nuevo

--- routes/song.routes.js
+++ routes/song.routes.js
@@ -3,8 +3,7 @@ const router = express.Router()
 const JamendoApiHandler = require('../services/APIHandler')
 const JamendoApi = new JamendoApiHandler()
 
-//Formato para busqueda por id de artista
-//"https://api.jamendo.com/v3.0/tracks?id=123447&lang=fr&client_id=ae3ce9b7"
+
 router.get('/:albumId', (req, res) => {
 
     JamendoApi.getAlbumDetails(req.params.albumId)
