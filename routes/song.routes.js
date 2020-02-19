328a0f0af4d84e1aedd5ca19a86f9f30b160fdd2
Luz Divina Delgado Castañeda <luzdivdelg@gmail.com>
2020-02-19 18:46:53 +0100

    updated

--- routes/song.routes.js
+++ routes/song.routes.js
@@ -3,21 +3,19 @@ const router = express.Router()
 const JamendoApiHandler = require('../services/APIHandler')
 const JamendoApi = new JamendoApiHandler()
 
-//Formato para busqueda por id de artista
-//"https://api.jamendo.com/v3.0/tracks?id=123447&lang=fr&client_id=ae3ce9b7"
 router.get('/:albumId', (req, res) => {
 
     JamendoApi.getAlbumDetails(req.params.albumId)
-        .then(songFound =>{
+        .then(songFound => {
             console.log(songFound)
-            res.render('new-songs', { infoAlbum: songFound })})
+            res.render('new-songs', {
+                infoAlbum: songFound
+            })
+        })
 
-            
+
         .catch(err => console.log("ha habido un problema al encontrar la canción", err))
 
 })
-
-
-
 
 module.exports = router
