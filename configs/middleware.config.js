277ce6734354f1b151aacf2c5ad1f151c11a98e3
Sara Garzon <saragarzonburgos@gmail.com>
2020-02-20 11:31:19 +0100

    button

--- configs/middleware.config.js
+++ configs/middleware.config.js
@@ -4,17 +4,9 @@ const logger = require('morgan');
 
 module.exports = app => {
     app.use(logger('dev'))
-    app.use(function (req, res, next) {
-        console.log('REQ.BODY AMTES DE BODY PARSER', req.body)
-        next();
-    })
     app.use(bodyParser.json())
     app.use(bodyParser.urlencoded({
         extended: false
     }))
-    app.use(function (req, res, next) {
-        console.log('REQ.BODY DESPUES DE BODY PARSER', req.body)
-        next();
-    })
     app.use(cookieParser())
 }
