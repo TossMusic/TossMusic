24f4134020f10be7af63b62a483745fb93d764de
Sara Garzon <saragarzonburgos@gmail.com>
2020-02-20 10:25:40 +0100

    day 3 finished

--- configs/middleware.config.js
+++ configs/middleware.config.js
@@ -4,9 +4,17 @@ const logger = require('morgan');
 
 module.exports = app => {
     app.use(logger('dev'))
+    app.use(function (req, res, next) {
+        console.log('REQ.BODY AMTES DE BODY PARSER', req.body)
+        next();
+    })
     app.use(bodyParser.json())
     app.use(bodyParser.urlencoded({
         extended: false
     }))
+    app.use(function (req, res, next) {
+        console.log('REQ.BODY DESPUES DE BODY PARSER', req.body)
+        next();
+    })
     app.use(cookieParser())
 }
